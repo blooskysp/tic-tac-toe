609be7956e9dfa84c1353f19b52eb2b1f3a523ad
Maksim <nikoflsad@gmail.com>
2025-08-19 18:55:34 +0300

    mini-optimisation

--- src/Components/Game/Field/Field.jsx
+++ src/Components/Game/Field/Field.jsx
@@ -2,12 +2,18 @@ import styles from "./Field.module.css";
 import {useDispatch, useSelector} from "react-redux";
 import {setField} from "../../../actions/index.js";
 import {selectFields} from "../../../selectors/select-fields.js";
+import {selectDraw} from "../../../selectors/select-draw.js";
+import {selectGameEnd} from "../../../selectors/select-gameEnd.js";
 
 export default function FieldLayout() {
 	const fields = useSelector(selectFields);
+	const draw = useSelector(selectDraw);
+	const gameEnd = useSelector(selectGameEnd);
   const dispatch = useDispatch();
 
   const putMove = (i) => {
+		if (draw || gameEnd) return;
+
     dispatch(setField(i));
   };
 
